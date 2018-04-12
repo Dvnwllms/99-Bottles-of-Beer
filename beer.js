0e533f66d2a71d8fc36743da704b148c3a5b10f4
Devon Williams <dvnwllms009@gmail.com>
2018-04-12 10:02:15 -0400

    JavaScriptDone

---
+++ beer.js
@@ -0,0 +1,14 @@
+for(i = 99; i >= 0; i--) {
+    if (i === 1) {
+        console.log(i + " bottle of beer on the wall, " + i + " bottle of beer. Take one down pass it around...");
+    } else if (i === 0) {
+        console.log("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.")
+    } else if (i === 99) {
+        console.log(i + " bottles of beer on the wall, " + i + " bottles of beer. Take one down pass it around...");
+    } else {
+        console.log(i + " bottles of beer on the wall. " + i + " bottles of beer. Take one down pass it around..." )
+    }
+    
+
+
+}
